64639554900b4ddccf680babc03323c718157a80
el_codee <elfrand3@gmail.com>
2023-03-03 09:41:59 +0700

    pelatihan mobile app with flutter

---
+++ lib/loginform.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:pelatihan_flutter/homepage.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => new _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  TextEditingController controllerusername = new TextEditingController();
+  TextEditingController controllerpassword = new TextEditingController();
+  void error(BuildContext context, String error) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: Text(error),
+        action: SnackBarAction(
+            label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                Image(image: AssetImage("images/login.png")),
+                Text(
+                  "Login Form",
+                  style: TextStyle(fontFamily: "Netflix", fontSize: 20),
+                ),
+                new TextField(
+                  controller: controllerusername,
+                  style: TextStyle(fontFamily: "Netflix"),
+                  decoration: new InputDecoration(
+                      hintText: "Username", labelText: "Username"),
+                ),
+                new TextField(
+                  style: TextStyle(fontFamily: "Netflix"),
+                  controller: controllerpassword,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  decoration: new InputDecoration(
+                      hintText: "Password", labelText: "Password"),
+                ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                ),
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  child: new ElevatedButton(
+                    onPressed: () {
+                      if (controllerusername.value.text == 'admin' &&
+                          controllerpassword.value.text == 'admin') {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) {
+                          return HomePage();
+                        }));
+                        // _sendDataToSecondScreen(context);
+                      } else {
+                        error(context, "Username dan password salah!");
+                      }
+                    },
+                    child: new Text(
+                      "Login",
+                      style:
+                          TextStyle(fontFamily: "Netflix", color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
